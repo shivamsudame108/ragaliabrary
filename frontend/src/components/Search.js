36fb8506b3e914804facc05e93434295f4895616
shivamsudame108 <shivamsudame108@gmail.com>
2024-10-02 22:37:34 -0700

    Create Search.js

---
+++ frontend/src/components/Search.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Search = () => {
+  return (
+    <div>
+      <h2 style={{ textAlign: 'center' }}>Search</h2>
+      <input type="text" placeholder="Search for ragas or talas..." />
+      {/* Implement search functionality here */}
+    </div>
+  );
+};
+
+export default Search;
